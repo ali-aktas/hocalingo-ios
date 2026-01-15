ff69c83e90581e5273d06bd67c8d14707bdc0a16
ali-aktas <aliaktas07@outlook.com>
2026-01-15 04:02:57 +0300

    Initial Commit

---
+++ HocaLingo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HocaLingo
+//
+//  Created by Auralian on 15.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HocaLingo/HocaLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HocaLingoApp.swift
+//  HocaLingo
+//
+//  Created by Auralian on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HocaLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
